3242f2a5fee9ceffdb21d69a5cca88cd4464c08d
Mohammad Rasheed <rasheed.au@gmail.com>
2019-09-11 15:43:23 +0530

    null concept class

---
+++ src/test/java/nullconcept/NullConceptClass.java
@@ -0,0 +1,56 @@
+package nullconcept;
+
+public class NullConceptClass {
+
+	static Object ob;
+	static String str;
+	static NullConceptClass newObj;
+
+	public static void main(String[] args) {
+
+		// 7.casting
+		String s = (String) null;
+		System.out.println(s + "123");
+		System.out.println(s.charAt(0));// null pointer exception
+		System.out.println(s.length());// null pointer exception
+
+		// 6. comparision System.out.println(null == null);// true
+		System.out.println(null != null);// false
+
+		// 5. static vs non static
+		NullConceptClass obj1 = null;
+		obj1.funcStatic();// will work if function is static
+		obj1.funcNonStatic();// null pointer exception if function is non static
+
+		// 4. instanceof
+
+		Integer num1 = null;
+		Integer num2 = 10;
+		System.out.println(num2 instanceof Integer);
+		System.out.println(num1 instanceof Integer);
+
+		// 3. null can't be assinged to any primitive data type
+
+		Integer i = null;
+		int j = i;// NullPointerException System.out.println(j);
+
+		// 2. Default value of any/Object reference is null
+
+		System.out.println(ob);
+		System.out.println(str);
+		System.out.println(newObj);
+
+		// 1. Case sensitive
+
+		Object obj = null;
+
+	}
+
+	public static void funcStatic() {
+		System.out.println("in function 1");
+	}
+
+	public void funcNonStatic() {
+		System.out.println("in function 2");
+	}
+}
